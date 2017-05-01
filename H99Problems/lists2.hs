6bf40f8808247ac678ccdad90e66f65c0580cb0c
JHDillinger <armin.bernstetter@gmail.com>
2017-05-01 20:48:20 +0200

    forgot to add lists2.hs

---
+++ H99Problems/lists2.hs
@@ -0,0 +1,8 @@
+-- Problems 11 to 20 of the 99 Problems in Haskell
+-- https://wiki.haskell.org/99_questions/11_to_20
+
+main = print "hello"
+--11 Modified run-length encoding.
+-- Modify the result of problem 10 that if an element has no duplicates
+-- it is simply copied into the result list.
+-- Only elements with duplicates are transferred as (N E) lists.
