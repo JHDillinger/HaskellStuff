0500cfae38a13f18566d62a3447e46504cbd5dc5
JHDillinger <armin.bernstetter@gmail.com>
2017-05-14 23:54:48 +0200

    asdf

--- H99Problems/lists3.hs
+++ H99Problems/lists3.hs
@@ -3,3 +3,8 @@ main :: IO ()
 main = print "lists3"
 -- 21 insert an element at a given position into a list
 -- insertAt :: a -> [a] -> Int -> [a]
+
+
+-- 22 create a list containing all integers within a given range
+range :: Int -> Int -> [Int]
+range i k = [i..k]
--- sqroot.hs
+++ sqroot.hs
@@ -10,28 +10,45 @@ main = do
     putStrLn "Enter the eps value:"
     eps <- readLn
     putStrLn "The approximated square root is:"
-    print (sqroot a0 eps n)
+    print (relativesqroot a0 eps n)
 
-sqroot :: (Ord a, Fractional a) => a -> a -> a -> a
-sqroot a0 eps n = within eps (nrepeat (next n) a0)
+-- relativesqrt :: Double -> Double -> Double -> Double
+relativesqroot = sqroot relative
 
-relativesqroot :: (Ord a, Fractional a) => a -> a -> a -> a
-relativesqroot a0 eps n = relative eps (nrepeat (next n) a0)
+-- withinsqroot :: Double -> Double -> Double -> Double
+withinsqroot = sqroot within
 
+-- another abstraction layer
+sqroot method a0 eps n = method eps (repeat' (next n) a0)
+
+-- like in the paper:
+-- sqroot :: (Ord a, Fractional a) => a -> a -> a -> a
+-- sqroot a0 eps n = within eps (nrepeat (next n) a0)
+
+-- relativesqroot :: (Ord a, Fractional a) => a -> a -> a -> a
+-- relativesqroot a0 eps n = relative eps (nrepeat (next n) a0)
+
+
+-- creates a_i+1 from a_i (no "general purpose" function; only for this example)
 next :: (Fractional a) => a -> a -> a
 next n x = (x + n/x)/2
 
-nrepeat :: (a -> a) -> a -> [a]
-nrepeat f a = a:nrepeat f (f a)
+-- creates an infinite array of repeated applications of f
+-- implemented as iterate in Prelude
+repeat' :: (a -> a) -> a -> [a]
+repeat' f a = a : repeat' f (f a)
 
 within :: (Num a, Ord a) => a -> [a] -> a
 within _ [] = 0
-within eps (a:b:rest)
-    | abs (a-b) <= eps = b
-    | otherwise = within eps (b:rest)
+within _ [x] = x
+within eps (a:b:rest)   | abs (a-b) <= eps = b
+                        | otherwise = within eps (b:rest)
 
 relative :: (Fractional a, Ord a) => a -> [a] -> a
 relative _ [] = 0
-relative eps (a:b:rest)
-    | abs (a/b-1) <= eps = b
-    | otherwise = relative eps (b:rest)
+relative _ [x] = x
+relative eps (a:b:rest) | abs (a/b-1) <= eps = b
+                        | otherwise = relative eps (b:rest)
+
+test :: a -> [a] -> a
+test eps list = eps
